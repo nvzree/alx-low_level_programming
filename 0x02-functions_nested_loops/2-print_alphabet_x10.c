eaad3478ec62d5b4f2ce06f7dbc5253b256a4783
nvzree <davidnazree@gmail.com>
2023-08-15 16:03:02 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/** 
+ * print_alphabet_x10 - prints 10 times the alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int counter;
+
+	counter = 1;
+	while (counter <= 10)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		counter++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -7,13 +8,15 @@
  * Return: On success 1.
  * On error, -1 is returned, and error is set appropriately.
  */
-
 int _putchar(char c);
 
 /**
  * print_alphabet - print lowercase alphabet
  */
-
 void print_alphabet(void);
 
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ */
+void print_alphabet_x10(void);
 #endif
