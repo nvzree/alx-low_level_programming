4d3a6ec7b467bc386c9d8786c0729400d0d33206
nvzree <davidnazree@gmail.com>
2023-08-15 14:37:36 -0700

    initial commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,19 @@
- int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
+void print_alphabet(void);
+
+#endif
