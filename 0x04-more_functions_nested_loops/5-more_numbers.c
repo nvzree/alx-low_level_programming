405a14d8b441fc8a069cebe6d677cd4d690451fd
nvzree <davidnazree@gmail.com>
2023-08-17 12:01:40 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 - 14, ten times
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			if (j / 10 > 0)
+			{
+				_putchar('0' + (j / 10));
+			}
+
+			_putchar((j % 10) + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+
+}
