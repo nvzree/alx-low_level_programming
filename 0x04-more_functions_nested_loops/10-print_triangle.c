dfac1796ef4516c16e3146d6cc38a28b0c2f034e
nvzree <davidnazree@gmail.com>
2023-08-18 11:41:31 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int l;
+	int b;
+	int draw;
+
+	if ( size > 0)
+	{
+		for (l = 1; l <= size; l++)
+		{
+			for (b = 1; b <= (size - l); b++)
+			{
+				_putchar(' ');
+			}
+			for (draw = 1; draw <= l; draw++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
