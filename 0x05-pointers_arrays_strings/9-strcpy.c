ac09e619b29bd5e5061f4c11a516c96c315c2a15
nvzree <davidnazree@gmail.com>
2023-08-22 15:56:30 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string
+ * @dest: string pointer
+ * @src: string pointer
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	int i;
+
+	for (len = 0; len != '\0'; len++)
+	{
+
+	}
+
+	for (i = 0; i <= src[len]; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
