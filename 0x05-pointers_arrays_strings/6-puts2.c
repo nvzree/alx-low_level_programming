3a6b40ce0578039807a1d15e5d099580eb45a6a2
nvzree <davidnazree@gmail.com>
2023-08-22 11:51:22 -0700

    optimized code lines

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] % 2 == 0)
-			_putchar(str[i]);
+	for (i = 0; str[i] != '\0'; i+=2)
+	{	
+		_putchar(str[i]);
 	}
 
 	_putchar('\n');
