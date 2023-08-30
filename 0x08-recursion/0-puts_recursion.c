79c89649c3b4adbd8dac1efaa28b35f409db72c3
nvzree <davidnazree@gmail.com>
2023-08-30 08:11:28 -0700

    initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: pointer to char
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+	if (*s == '\0')
+		_putchar('\n');
+}
