c2142e462f39c0f9b55ea33d028de7cea6fc9c44
nvzree <davidnazree@gmail.com>
2023-09-01 15:02:09 -0700

    initial commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print number of arguments passed to program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
