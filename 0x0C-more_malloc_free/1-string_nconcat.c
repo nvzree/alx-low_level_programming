ccf6c59c5d3f04360dd237b3ddf36bc5b490c7ed
nvzree <davidnazree@gmail.com>
2023-09-08 12:04:17 -0700

    initial commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,6 +18,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	if (n >= strlen(s2))
+		n = strlen(s2);
+	if (n < 0)
+		return (NULL);
 
 	new_concat = malloc(sizeof(char) * (len1 + n + 1));
 	if (new_concat == NULL)
