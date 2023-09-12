3199af73952730088b46e60a3a82d3f054c4fb2f
nvzree <davidnazree@gmail.com>
2023-09-12 16:45:31 -0700

    init commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,14 +16,14 @@ int len(char *str)
 	return (i);
 }
 /**
- * strcpy - copies the string pointed to by src,
+ * _strcpy - copies the string pointed to by src,
  * including the terminating null byte (\0),
  * to the buffer pointed to by dest
  * @dest: copy source to this buffer
  * @src: this is the source to copy
  * Return: copy of original source
  */
-char *strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i;
 
@@ -60,7 +60,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(dog1);
 			return (NULL);
 		}
-		dog1->name = strcpy(copy_of_name, name);
+		dog1->name = _strcpy(copy_of_name, name);
 	}
 	else
 		dog1->name = NULL;
@@ -74,7 +74,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(dog1);
 			return (NULL);
 		}
-		dog1->owner = strcpy(copy_of_owner, owner);
+		dog1->owner = _strcpy(copy_of_owner, owner);
 	}
 	else
 		dog1->owner = NULL;
