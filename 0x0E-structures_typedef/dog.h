26110f29ebfef49224b3aa4e6fa1b49dc4812952
nvzree <davidnazree@gmail.com>
2023-09-12 13:07:14 -0700

    initial commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - short info about dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: pointer to string
+ */
+
 struct dog
 {
 	char *name;
