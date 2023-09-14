dfa117c9c7b024e9d366c7a46be23bac9828dc53
nvzree <davidnazree@gmail.com>
2023-09-14 14:43:16 -0700

    initial commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * n: number of parameters
+ * Return: int sum of parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i;
+	int sum;
+	
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n); /*initializing argument pointer*/
+
+	sum = 0;
+	for (i = 0; i < (int)n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,21 @@
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
+
+int sum_them_all(const unsigned int n, ...);
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+#endif
