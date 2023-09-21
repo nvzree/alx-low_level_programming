f60c1c72d7f1b30ed28198ba221e87e0f36f5cca
nvzree <davidnazree@gmail.com>
2023-09-21 15:54:37 -0700

    initial commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * _strlen - calculates the length of string
+ * @str: string
+ * Retrun: length of string
+ */
+
+int _strlen(const char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		
+	}
+	return len;
+}
+
+/**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to first node
  * @str: data for new node
@@ -21,7 +38,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = _strlen(str);
 
 	if (head == NULL)
 		new->next = NULL;
