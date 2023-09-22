75120a3c21a68e6a687b3133ebe0299ce1c67f36
nvzree <davidnazree@gmail.com>
2023-09-22 03:33:52 -0700

    advanced

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-global _start
-
-section .text
-
-_start:
-  mov rax, 1        ; write(
-  mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, msg      ;   "Hello, world!\n",
-  mov rdx, msglen   ;   sizeof("Hello, world!\n")
-  syscall           ; );
-
-  mov rax, 60       ; exit(
-  mov rdi, 0        ;   EXIT_SUCCESS
-  syscall           ; );
-
-section .rodata
-  msg: db "Hello, Holberton", 10
-  msglen: equ $ - msg
+global	main
+	extern	printf
+main:
+	mov	edi,  format
+	xor	eax,  eax
+	call	printf
+	mov 	eax, 0
+	ret
+format:  db `Hello, Holberton\n`,0
