874d45614ebe9d23f452f330641d4d7095f963a5
nvzree <davidnazree@gmail.com>
2023-09-25 12:06:17 +0000

    initial commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * h@: pointer to struct
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t num_count = 0;
+
+	while (h != NULL)
+	{
+		num_count++;
+		h = h->next;
+	}
+	return (num_count);
+}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: double pointer to struct
+ * @n: node count
+ * Return: adress of the new element, or NULL if failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next= *head;
+
+	*head = new_node; /* set head to point to new node*/
+
+	return (new_node);
+}
